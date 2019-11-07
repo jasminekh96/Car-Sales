56ebff02b24918ad85b119f0ac2f473c2540d60f
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-11-06 22:49:26 -0500

    Add and Remove functions

---
+++ src/reducers/index.js
@@ -0,0 +1,44 @@
+import { createStore } from 'redux';
+import { ADD_ITEM, REMOVE_ITEM } from '../actions/index';
+
+const initialState = {
+	additionalPrice    : 0,
+	car                : {
+		price    : 26395,
+		name     : '2019 Ford Mustang',
+		image    : 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+		features : [],
+	},
+	additionalFeatures : [
+		{ id: 1, name: 'V-6 engine', price: 1500 },
+		{ id: 2, name: 'Racing detail package', price: 1500 },
+		{ id: 3, name: 'Premium sound system', price: 500 },
+		{ id: 4, name: 'Rear spoiler', price: 250 },
+	],
+};
+
+export const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case ADD_ITEM:
+			return {
+				...state,
+				car : {
+					...state.car,
+					features : [ ...state.car.features, action.payload ],
+				},
+			};
+		case REMOVE_ITEM:
+			return {
+				...state,
+				car : {
+					...state.car,
+					// does feature === item (action.payload);
+					// feature !== item
+					features : state.car.features.filter((feature) => feature.id !== action.payload.id),
+				},
+			};
+		default:
+			return state;
+	}
+};
+export const store = createStore(reducer);
